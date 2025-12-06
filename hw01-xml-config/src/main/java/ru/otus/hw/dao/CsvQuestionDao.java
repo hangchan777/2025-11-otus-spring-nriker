1afb0f6c611c3dad27d91e877d714e0b76608227
hangchan777 <58440707+hangchan777@users.noreply.github.com>
2025-12-07 00:28:26 +0300

    Added Homework 1 from template hw01-xml-config: printing questions and answers from questions.csv file, added executable jar.
    Added printing questions file
    
    Parsing csv file
    
    Printing csv file. Added new questions
    
    Added executable jar.

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,23 +1,45 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
-    private final TestFileNameProvider fileNameProvider;
+	private final TestFileNameProvider fileNameProvider;
 
-    @Override
-    public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+	@Override
+	public List<Question> findAll() {
+		try (InputStream inputStream = getClass().getResourceAsStream(fileNameProvider.getTestFileName())) {
+			Reader reader = new InputStreamReader(Objects.requireNonNull(inputStream));
+			var strategy = new ColumnPositionMappingStrategy<QuestionDto>();
+			strategy.setType(QuestionDto.class);
+			var csvToBean = new CsvToBeanBuilder<QuestionDto>(reader)
+					.withMappingStrategy(strategy)
+					.withSeparator(';')
+					.withSkipLines(1)
+					.withIgnoreLeadingWhiteSpace(true)
+					.build();
+			return getQuestionsFromQuestionDto(csvToBean.parse());
+		} catch (Exception e) {
+			throw new QuestionReadException("Error reading the questions file", e);
+		}
+	}
 
-        return new ArrayList<>();
-    }
+	private List<Question> getQuestionsFromQuestionDto(List<QuestionDto> questionDtoList) {
+		var questions = new ArrayList<Question>();
+		questionDtoList.forEach(questionDto -> questions.add(questionDto.toDomainObject()));
+		return questions;
+	}
 }
