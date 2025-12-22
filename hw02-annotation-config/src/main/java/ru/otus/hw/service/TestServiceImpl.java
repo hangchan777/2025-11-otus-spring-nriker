affb04b42c94b39981d6beea3a82a3a30b541f78
hangchan777 <58440707+hangchan777@users.noreply.github.com>
2025-12-23 01:02:40 +0300

    HW02: рабочая программа для тестирования. Без UT

--- hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,8 +3,15 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
+import ru.otus.hw.exceptions.UserAnswerReadException;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @RequiredArgsConstructor
@@ -22,9 +29,37 @@ public class TestServiceImpl implements TestService {
         var testResult = new TestResult(student);
 
         for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+            printQuestion(question);
+            var isAnswerAccepted = new AtomicBoolean(false);
+            var isAnswerValid = false;
+            while (!isAnswerAccepted.get()) {
+                try {
+                    var userAnswer = ioService.readStringWithPrompt("Your answer?");
+                    isAnswerValid = checkAnswer(question.answers(), userAnswer);
+                    isAnswerAccepted.set(true);
+                } catch (UserAnswerReadException e) {
+                    ioService.printFormattedLine("Your answer does not match the answer options. Please, try again");
+                }
+            }
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
+    }
+
+    private void printQuestion(Question question) {
+        ioService.printFormattedLine(question.text());
+        AtomicInteger questionNumber = new AtomicInteger(1);
+        question.answers().forEach(answer -> {
+            ioService.printFormattedLine(questionNumber.toString() + ". " + answer.text());
+            questionNumber.getAndIncrement();
+        });
+    }
+
+    private boolean checkAnswer(List<Answer> answers, String userAnswer) {
+        var answerResult = answers.stream().filter(answer -> answer.text().equals(userAnswer)).findFirst();
+        if (answerResult.isPresent()) {
+            return answerResult.get().isCorrect();
+        }
+        throw new UserAnswerReadException("An inappropriate response was entered");
     }
 }
