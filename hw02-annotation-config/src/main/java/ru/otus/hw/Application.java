9c3e1447a87c4f76fd0717163d7389471ab3b4a5
hangchan777 <58440707+hangchan777@users.noreply.github.com>
2025-12-23 18:24:54 +0300

    HW02: рефакторинг

--- hw02-annotation-config/src/main/java/ru/otus/hw/Application.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/Application.java
@@ -2,9 +2,11 @@ package ru.otus.hw;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import ru.otus.hw.service.TestRunnerService;
 
 @ComponentScan
+@PropertySource("application.properties")
 public class Application {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(Application.class);
--- hw02-annotation-config/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -2,17 +2,19 @@ package ru.otus.hw.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
-@PropertySource("application.properties")
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
-    @Value("${test.rightAnswersCountToPass}")
-    private int rightAnswersCountToPass;
+    private final int rightAnswersCountToPass;
 
-    @Value("${test.fileName}")
-    private String testFileName;
+    private final String testFileName;
+
+    public AppProperties(@Value("${test.rightAnswersCountToPass}") int rightAnswersCountToPass,
+                         @Value("${test.fileName}") String testFileName) {
+        this.rightAnswersCountToPass = rightAnswersCountToPass;
+        this.testFileName = testFileName;
+    }
 }
