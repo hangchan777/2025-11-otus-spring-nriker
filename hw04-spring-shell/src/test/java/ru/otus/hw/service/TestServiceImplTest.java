17162e726c2766c150a062121ef15b5e273be61a
hangchan777 <58440707+hangchan777@users.noreply.github.com>
2026-01-13 23:07:26 +0300

    HW04: добавлен модуль hw04. Добавлен spring shell. Переведены UT на SpringUnitTest

---
+++ hw04-spring-shell/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -0,0 +1,50 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
+import ru.otus.hw.utils.TestUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(properties = "spring.config.location=classpath:application-test.yml")
+class TestServiceImplTest {
+
+	Student student = new Student("FirstName", "LastName");
+	@MockitoBean
+	LocalizedIOService ioService;
+	@Autowired
+	TestServiceImpl testService;
+
+	@Test
+	void test_executeTestFor_studentTestSucceed() {
+		when(ioService.readIntForRangeWithPromptLocalized(anyInt(), anyInt(), anyString(), anyString())).thenReturn(1);
+		var expectedTestResult = getExpectedTestResult(student, true);
+
+		var actualTestResult = testService.executeTestFor(student);
+
+		assertEquals(expectedTestResult, actualTestResult);
+	}
+
+	@Test
+	void test_executeTestFor_studentTestFailed() {
+		when(ioService.readIntForRangeWithPromptLocalized(anyInt(), anyInt(), anyString(), anyString())).thenReturn(2);
+		var expectedTestResult = getExpectedTestResult(student, false);
+
+		var actualTestResult = testService.executeTestFor(student);
+
+		assertEquals(expectedTestResult, actualTestResult);
+	}
+
+	private TestResult getExpectedTestResult(Student student, boolean result) {
+		var testResult = new TestResult(student);
+		testResult.applyAnswer(TestUtils.getExpectedQuestions().get(0), result);
+		return testResult;
+	}
+}
